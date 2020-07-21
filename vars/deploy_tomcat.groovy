95e6c7491bdaab2cc3741875bbe37db422e3830a
KollaPriyanka <55626247+KollaPriyanka@users.noreply.github.com>
2020-07-21 16:30:37 +0530

    Update deploy_tomcat.groovy

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -2,5 +2,5 @@ def call(tomcaturl) {
 def urll = tomcaturl
 //sh 'curl ${urll} -u tomcat:secret'
 //sh 'curl -v -u tomcat:secret -T target/BMI-0.war ${urll}'
-deploy adapters: [tomcat8(credentialsId: 'tomcat_cred', path: '', url: ${urll})], contextPath: 'shared_library', onFailure: false, war: '**/*.war'
+deploy adapters: [tomcat8(credentialsId: 'tomcat_cred', path: '', url: '${urll}')], contextPath: 'shared_library', onFailure: false, war: '**/*.war'
             }
