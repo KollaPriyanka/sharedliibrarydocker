c8ded30056d82a2c3e22f4a9ac54a79936e66a03
KollaPriyanka <55626247+KollaPriyanka@users.noreply.github.com>
2020-07-22 16:15:53 +0530

    Update deploy_tomcat.groovy

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -1,4 +1,4 @@
 def call(String msg = 'deploy_dev') {
 echo "${msg}"
- deploy adapters: [tomcat8(credentialsId: 'tomcat_cred', path: '', url: 'http://3.133.100.227:8080')], contextPath: 'shared_library', onFailure: false, war: '**/*.war'
+ deploy adapters: [tomcat8(credentialsId: 'tomcat_cred', path: '', url: 'http://18.220.127.250:8080')], contextPath: 'shared_library', onFailure: false, war: '**/*.war'
             }
